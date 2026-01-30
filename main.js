3a7eac957df87c3db91814932a37551efa48dc7a
jzapdos <226jmanley@frhsd.com>
2026-01-30 09:46:24 -0500

    Computed Properties

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const app = Vue.createApp({
     data: function() {
         return {
             cart:0,
-            image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             product:'Socks',
-            inStock: false,
+            brand: 'Vue Mastery',
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', },
-                { id: 2335, color: 'blue', image: './assets/images/socks_blue.jpg', },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                { id: 2335, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -16,8 +16,19 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
         }
+    },
+    computed:{
+        title() {
+            return this.brand + ' ' +this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
     }
 })
