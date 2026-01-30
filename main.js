65a242f9ffe1c85aab73592c20d78a6bf32ebb59
jzapdos <226jmanley@frhsd.com>
2026-01-30 09:59:21 -0500

    Components & Props

--- main.js
+++ main.js
@@ -2,33 +2,10 @@ const app = Vue.createApp({
     data: function() {
         return {
             cart:0,
-            selectedVariant: 0,
-            product:'Socks',
-            brand: 'Vue Mastery',
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
-                { id: 2335, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ]
+            premium: true
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-        }
+
     },
-    computed:{
-        title() {
-            return this.brand + ' ' +this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-    }
 })
